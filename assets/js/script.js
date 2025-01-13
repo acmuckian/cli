3b778d58bb91523969de23b65bfc8fa5e811279a
Aoife Muckian <dhv3af1l@students.codeinstitute.net>
2025-01-13 20:41:17 +0000

    added displaystatus function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_KEY = "--kKjpZiw93h4lG-2739keRmeKA";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
@@ -13,7 +13,19 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayData(data);
+    } else {
+        throw new Error(data.error);
     }
 
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results =`<div>Your key is valid until</div>`;
+    results += `<div class="key-status"><${data.expiry}/div>`;
+    document.getElementById("resultsModalTitle").innerText = HTMLHeadingElement;
+    document.getElementById("results-content").innerHTML = results;
+    resultModal.show();
 }
