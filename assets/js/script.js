2c7f235e48bc7d0b37d1db3d536bd3ce43aeae4b
Aoife Muckian <dhv3af1l@students.codeinstitute.net>
2025-01-13 20:21:39 +0000

    testing error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-const API_Key = "--kKjpZiw93h4lG-2739keRmeKA";
-const API_URL = "https://ci-jshint.herokuapp.com/";
-const resultsModal = new bootstrap.modal(document.getElementById("resultsModal"));
+const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString =`${API_URL}?api_key=${API_KEY}`;
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
     const data = await response.json();
-}
 
-if (response.ok) {
-    console.log(data);
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
 }
